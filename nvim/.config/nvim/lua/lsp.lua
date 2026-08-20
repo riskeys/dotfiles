2f00e884271328566cd6e698b10db05676a667b1
riskeys <riskeys32@gmail.com>
2026-08-20 19:15:55 +0700

    enable several lsps

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,5 +1,10 @@
+vim.lsp.enable('tailwindcss')
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('denols')
+vim.lsp.enable('go')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('zls')
+-- vim.lsp.enable('csharp_ls')
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
@@ -8,11 +13,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
     end
 
-    map('n', 'K', vim.lsp.buf.hover, 'LSP Hover')
+		local buf_hover = function ()
+			vim.lsp.buf.hover({
+				border = "rounded"
+			})
+		end
+
+    map('n', 'K', buf_hover, 'LSP Hover')
     map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
     map('n', 'gD', vim.lsp.buf.declaration, 'Go to declaration')
     map('n', 'gI', vim.lsp.buf.implementation, 'Go to implementation')
     map('n', 'gr', vim.lsp.buf.references, 'References')
+    map('n', 'ga', vim.lsp.buf.code_action, 'Code Action')
+    map('n', 'gl', vim.diagnostic.open_float, 'Inline diagnostic')
     map('n', '<leader>rn', vim.lsp.buf.rename, 'Rename symbol')
     map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, 'Code action')
     map('n', '<leader>gf', function()
@@ -28,7 +41,7 @@ local cmp = require('blink.cmp')
 -- cmp.build():wait(60000)
 cmp.setup({
 	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
+		default = { "lsp", "path", "snippets" },
 		providers = {
 			-- cmdline = { min_keyword_length = 2, },
 			cmdline = {},
