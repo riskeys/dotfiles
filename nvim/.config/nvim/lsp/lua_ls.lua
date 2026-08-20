b203e44390c38281bfe30f1df86179f48bcb5f60
riskeys <riskeys32@gmail.com>
2026-08-20 19:14:12 +0700

    add deno and lua ls

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -1,17 +1,38 @@
+local root_markers1 = {
+  '.emmyrc.json',
+  '.luarc.json',
+  '.luarc.jsonc',
+}
+local root_markers2 = {
+  '.luacheckrc',
+  '.stylua.toml',
+  'stylua.toml',
+  'selene.toml',
+  'selene.yml',
+}
+
 return {
 	cmd = { 'lua-language-server' },
+	filetypes = { 'lua' },
+	root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2, { '.git' } }
+	or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { '.git' }),
 	settings = {
 		Lua = {
 			diagnostics = {
-				globals = { "vim" }
-			}
-		}
-	},
-	workspace = {
-		checkThirdParty = false,
-		library = {
-			vim.env.VIMRUNTIME,
+				globals = { 'vim' },
+			},
+			runtime = {
+				version = 'LuaJIT',
+			},
+			workspace = {
+				library = {
+					vim.fn.expand("$VIMRUNTIME/lua"),
+					vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
+				},
+			},
+			codeLens = { enable = true },
+			hint = { enable = true, semicolon = 'Disable' },
 		},
-	},
 	telemetry = { enable = false },
+	},
 }
